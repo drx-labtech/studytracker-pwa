d0d5596c895b9a91335f40be1c589a35017b195c
drx-labtech <veenoo12@gmail.com>
2026-02-27 02:20:16 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "studytracker-v1";
+const CACHE_NAME = "studytracker-v2";
 
 const ASSETS = [
   "./",
@@ -42,4 +42,5 @@ self.addEventListener("fetch", (event) => {
       return response || fetch(event.request);
     })
   );
+
 });
